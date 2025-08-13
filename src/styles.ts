22baea61f8d8f5bd46d6923b82dac862b8562f62
CesarSants <cesarsantosplay4@gmail.com>
2025-08-12 21:04:05 -0300

    done33

--- src/styles.ts
+++ src/styles.ts
@@ -62,5 +62,4 @@ export const Container = styled.div`
   /* Garantindo que não haja scroll horizontal */
   max-width: 100vw;
   /* Garantindo que os elementos filhos não vazem */
-  contain: paint;
 `
